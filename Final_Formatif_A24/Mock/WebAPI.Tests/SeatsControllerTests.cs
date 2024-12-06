8df3b2e466c3c2dcf2e5a34c9e5762d06572552a
Louis-Philippe Viens <2034421@cegepmontpetit.ca>
2024-12-06 17:26:09 -0500

    start

--- Final_Formatif_A24/Mock/WebAPI.Tests/SeatsControllerTests.cs
+++ Final_Formatif_A24/Mock/WebAPI.Tests/SeatsControllerTests.cs
@@ -1,14 +1,36 @@
 ﻿using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using Moq;
+using System.Xml.Linq;
+using WebAPI.Controllers;
+using WebAPI.Data;
+using WebAPI.Services;
 
 namespace WebAPI.Tests;
 
 [TestClass]
 public class SeatsControllerTests
 {
+    [TestInitialize]
+    public void init()
+    {
+        string seatBD = Guid.NewGuid().ToString();
+        DbContextOptions<WebAPIContext> options = new DbContextOptionsBuilder<WebAPIContext>()
+                .UseInMemoryDatabase(databaseName: seatBD)
+                .UseLazyLoadingProxies(true)
+                .Options;
+
+    }
+
+
     [TestMethod]
     public void ReserveSeat()
     {
-        
+
+        var seatService = new Mock<SeatsService>();
+        Mock<SeatsController> seatController = new Mock<SeatsController>() { CallBase = true };
+
     }
 }
